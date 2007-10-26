f441b4593541b0ab5b306f4647811e7862ca050a
njs <njs@pobox.com>
2007-10-26 08:48:46 +0000

    defer version number intelligence

--- parti/scripts/__init__.py
+++ parti/scripts/__init__.py
@@ -3,4 +3,4 @@ from optparse import OptionParser
 
 def PartiOptionParser(**kwargs):
     version_num = ".".join(map(str, parti.__version__))
-    return OptionParser(version=("Parti %s" % version_num), **kwargs)
+    return OptionParser(version="Parti vFIXME", **kwargs)
