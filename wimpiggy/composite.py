9147ed96f869346dc0dcf66ba8795102104a96d2
njs <njs@pobox.com>
2008-03-28 06:38:14 +0000

    Correctly handle NameWindowPixmap invalidation.  Maybe.  This is very tricky code.  I can't tell if the new code is buggy, or I'm seeing other pre-existing bugs exposed by fixing the old NameWindowPixmap bugs.

--- wimpiggy/composite.py
+++ wimpiggy/composite.py
@@ -1,4 +1,5 @@
 import gobject
+import gtk
 from wimpiggy.util import one_arg_signal, AutoPropGObjectMixin
 from wimpiggy.error import *
 from wimpiggy.lowlevel import (xcomposite_redirect_window,
@@ -6,14 +7,15 @@ from wimpiggy.lowlevel import (xcomposite_redirect_window,
                                xcomposite_name_window_pixmap,
                                xdamage_start, xdamage_stop,
                                xdamage_acknowledge,
-                               add_event_receiver, remove_event_receiver)
+                               add_event_receiver, remove_event_receiver,
+                               get_parent, addXSelectInput, const)
 
 class CompositeHelper(AutoPropGObjectMixin, gobject.GObject):
     __gsignals__ = {
         "contents-changed": one_arg_signal,
 
         "wimpiggy-damage-event": one_arg_signal,
-        "wimpiggy-map-event": one_arg_signal,
+        "wimpiggy-unmap-event": one_arg_signal,
         "wimpiggy-configure-event": one_arg_signal,
         "wimpiggy-reparent-event": one_arg_signal,
         }
@@ -31,9 +33,9 @@ class CompositeHelper(AutoPropGObjectMixin, gobject.GObject):
         self._already_composited = already_composited
         if not self._already_composited:
             xcomposite_redirect_window(window)
+        self._listening_to = None
         self.invalidate_pixmap()
         self._damage_handle = xdamage_start(window)
-        self._ancestry_listening = None
 
         add_event_receiver(self._window, self)
 
@@ -43,10 +45,7 @@ class CompositeHelper(AutoPropGObjectMixin, gobject.GObject):
         trap.swallow(xdamage_stop, self._window, self._damage_handle)
         self._damage_handle = None
         self._contents_handle = None
-        if self._listening_to is not None:
-            for w in self._listening_to:
-                remove_event_receiver(w, self)
-            self._listening_to = None
+        self.invalidate_pixmap()
         remove_event_receiver(self._window, self)
         self._window = None
 
@@ -57,12 +56,48 @@ class CompositeHelper(AutoPropGObjectMixin, gobject.GObject):
 
     def invalidate_pixmap(self):
         print "invalidating named pixmap"
+        if self._listening_to is not None:
+            # Don't want to stop listening to self._window!:
+            assert self._window not in self._listening_to
+            for w in self._listening_to:
+                remove_event_receiver(w, self)
+            self._listening_to = None
         self._contents_handle = None
 
     def do_get_property_contents_handle(self, name):
         if self._contents_handle is None:
             print "refreshing named pixmap"
+            assert self._listening_to is None
             def set_pixmap():
+                # The tricky part here is that the pixmap returned by
+                # NameWindowPixmap gets invalidated every time the window's
+                # viewable state changes.  ("viewable" here is the X term that
+                # means "mapped, and all ancestors are also mapped".)  But
+                # there is no X event that will tell you when a window's
+                # viewability changes!  Instead we have to find all ancestors,
+                # and watch all of them for unmap and reparent events.  But
+                # what about races?  I hear you cry.  By doing things in the
+                # exact order:
+                #   1) select for StructureNotify
+                #   2) QueryTree to get parent
+                #   3) repeat 1 & 2 up to the root
+                #   4) call NameWindowPixmap
+                # we are safe.  (I think.)
+                listening = []
+                win = get_parent(self._window)
+                while win is not gtk.gdk.get_default_root_window():
+                    # We have to use a lowlevel function to manipulate the
+                    # event selection here, because SubstructureRedirectMask
+                    # does not roundtrip through the GDK event mask
+                    # functions.  So if we used them, here, we would clobber
+                    # corral window selection masks, and those don't deserve
+                    # clobbering.  They are our friends!  X is driving me
+                    # slowly mad.
+                    addXSelectInput(win, const["StructureNotifyMask"])
+                    add_event_receiver(win, self)
+                    listening.append(win)
+                    win = get_parent(win)
+                self._listening_to = listening
                 handle = xcomposite_name_window_pixmap(self._window)
                 self._contents_handle = handle
             trap.swallow(set_pixmap)
@@ -75,10 +110,13 @@ class CompositeHelper(AutoPropGObjectMixin, gobject.GObject):
         else:
             return handle.pixmap
 
-    def do_wimpiggy_map_event(self, *args):
+    def do_wimpiggy_unmap_event(self, *args):
         self.invalidate_pixmap()
 
     def do_wimpiggy_configure_event(self, *args):
+        self.invalidate_pixmap()
+
+    def do_wimpiggy_reparent_event(self, *args):
         self.invalidate_pixmap()
 
     def do_wimpiggy_damage_event(self, event):
