91154daa0a2ab1641ecad70448d84b7ae9b9a635
njs <njs@pobox.com>
2008-11-02 08:11:48 +0000

    bump version number; write NEWS for v0.0.5

--- wimpiggy/__init__.py
+++ wimpiggy/__init__.py
@@ -1,4 +1,4 @@
 import pygtk
 pygtk.require('2.0')
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
--- xpra/__init__.py
+++ xpra/__init__.py
@@ -1,6 +1,6 @@
 import pygtk
 pygtk.require('2.0')
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 import wimpiggy
 assert wimpiggy.__version__ == __version__
