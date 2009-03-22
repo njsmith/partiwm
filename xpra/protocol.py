1cfff81f4f8755c10d6297173e7e7fd3e6e43dc5
njs <njs@pobox.com>
2009-03-22 23:29:23 +0000

    use only one IO watcher, in a stab at win32 compatibility

--- xpra/protocol.py
+++ xpra/protocol.py
@@ -1,5 +1,5 @@
 # This file is part of Parti.
-# Copyright (C) 2008 Nathaniel Smith <njs@pobox.com>
+# Copyright (C) 2008, 2009 Nathaniel Smith <njs@pobox.com>
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -32,29 +32,37 @@ class Protocol(object):
         self.source = None
         self._source_has_more = False
         self._accept_packets = False
-        self._read_tag = gobject.io_add_watch(self._sock, gobject.IO_IN,
-                                              self._socket_readable)
-        self._write_tag = None
+        self._closed = False
+        self._write_armed = False
         self._read_buf = ""
         self._write_buf = ""
         self._compressor = None
         self._decompressor = None
+        self._watch_tag = None
+        self._update_watch(force=True)
 
     def source_has_more(self):
         self._source_has_more = True
-        self._update_write_watch()
+        self._update_watch()
 
-    def _update_write_watch(self):
-        want_write = bool(self._write_buf or self._source_has_more)
-        write_armed = bool(self._write_tag is not None)
-        if want_write == write_armed:
+    def _update_watch(self, force=False):
+        # It would perhaps be simpler to use two separate watches, one for
+        # reading and one for writing, but that doesn't work on Win32:
+        #   http://faq.pygtk.org/index.py?req=all#20.20
+        # Win32 also requires we watch for IO_HUP.
+        if self._closed:
             return
+        want_write = bool(self._write_buf or self._source_has_more)
+        if not force and want_write == self._write_armed:
+            return
+        flags = gobject.IO_IN | gobject.IO_HUP
         if want_write:
-            self._write_tag = gobject.io_add_watch(self._sock, gobject.IO_OUT,
-                                                   self._socket_writeable)
-        else:
-            gobject.source_remove(self._write_tag)
-            self._write_tag = None
+            flags |= gobject.IO_OUT
+        if self._watch_tag is not None:
+            gobject.source_remove(self._watch_tag)
+        self._watch_tag = gobject.io_add_watch(self._sock, flags,
+                                               self._socket_ready)
+        self._write_armed = want_write
 
     def _flush_one_packet_into_buffer(self):
         if not self.source:
@@ -69,16 +77,27 @@ class Protocol(object):
             else:
                 self._write_buf += data
 
-    def _connection_lost(self):
-        self._accept_packets = False
-        self._process_packet_cb(self, [Protocol.CONNECTION_LOST])
+    def _socket_ready(self, source, flags):
+        if flags & gobject.IO_IN:
+            self._socket_readable()
+        if flags & gobject.IO_OUT:
+            self._socket_writeable()
+        if flags & gobject.IO_HUP:
+            self._connection_lost()
+        return True
 
-    def _socket_writeable(self, *args):
+    def _connection_lost(self):
+        if not self._closed:
+            self._accept_packets = False
+            self._process_packet_cb(self, [Protocol.CONNECTION_LOST])
+            self.close()
+
+    def _socket_writeable(self):
         if not self._write_buf:
             # Underflow: refill buffer from source.
             # We can't get into this function at all unless either _write_buf
             # is non-empty, or _source_has_more == True, because of the guard
-            # in _update_write_watch.
+            # in _update_watch.
             assert self._source_has_more
             self._flush_one_packet_into_buffer()
         try:
@@ -88,10 +107,9 @@ class Protocol(object):
             self._connection_lost()
         else:
             self._write_buf = self._write_buf[sent:]
-            self._update_write_watch()
-        return True
+            self._update_watch()
 
-    def _socket_readable(self, *args):
+    def _socket_readable(self):
         try:
             buf = self._sock.recv(4096)
         except socket.error:
@@ -114,7 +132,6 @@ class Protocol(object):
                 self._read_buf = self._decompressor.decompress(self._read_buf)
             if consumed == 0:
                 break
-        return True
 
     def _consume_packet(self, data):
         try:
@@ -137,16 +154,15 @@ class Protocol(object):
         # Flush everything out of the source
         while self._source_has_more:
             self._flush_one_packet_into_buffer()
-        self._update_write_watch()
+        self._update_watch()
         # Now enable compression
         self._compressor = zlib.compressobj(level)
         self._decompressor = zlib.decompressobj()
 
     def close(self):
-        if self._read_tag is not None:
-            gobject.source_remove(self._read_tag)
-            self._read_tag = None
-        if self._write_tag is not None:
-            gobject.source_remove(self._write_tag)
-            self._write_tag = None
-        self._sock.close()
+        if not self._closed:
+            if self._watch_tag is not None:
+                gobject.source_remove(self._watch_tag)
+                self._watch_tag = None
+            self._closed = True
+            self._sock.close()
