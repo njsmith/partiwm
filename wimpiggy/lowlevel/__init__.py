de5d0a2224425a9fb49e21006b03ce93e3610730
njs <njs@pobox.com>
2009-03-24 21:03:33 +0000

    logging tweak

--- wimpiggy/lowlevel/__init__.py
+++ wimpiggy/lowlevel/__init__.py
@@ -9,13 +9,13 @@ from wimpiggy.log import Logger
 log = Logger()
 
 def send_wm_take_focus(target, time):
-    log("sending WM_TAKE_FOCUS")
+    log("sending WM_TAKE_FOCUS: %r, %r", target, time)
     sendClientMessage(target, False, 0,
                       "WM_PROTOCOLS",
                       "WM_TAKE_FOCUS", time, 0, 0, 0)
 
 def send_wm_delete_window(target):
-    log("sending WM_TAKE_FOCUS")
+    log("sending WM_DELETE_WINDOW")
     sendClientMessage(target, False, 0,
                       "WM_PROTOCOLS",
                       "WM_DELETE_WINDOW", const["CurrentTime"], 0, 0, 0)
