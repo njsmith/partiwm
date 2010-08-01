26b720e35dd87496ae6b3b6de5b6e1a9ac26d0a8
Nathaniel Smith <njs@pobox.com>
2010-08-01 07:51:11 -0700

    wrong name

--- xpra/platform/win32.py
+++ xpra/platform/win32.py
@@ -1,7 +1,7 @@
 XPRA_LOCAL_SERVERS_SUPPORTED = False
 DEFAULT_SSH_CMD = "plink"
 
-from xpra.platform.win32pipe import spawn_with_channel
+from xpra.platform.win32pipe import spawn_with_channel_socket
 
 from xpra.platform.win32clipboard import ClipboardProtocolHelper
 
